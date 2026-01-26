0cd9a530d0b7a83a1aebda0942bc25c22435e13c
isanvagui <isanvagui92@gmail.com>
2026-01-26 14:03:51 -0500

    fix: elimino el archivo app.py del gitignore y cambio la configuración a production

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from extensions import db, csrf, login_manager
 from routes import bp
 
 app = Flask(__name__, static_url_path='/mantenimientos-tecnologia/static')
-app.config.from_object(config['development'])
+app.config.from_object(config['production'])
 app.register_blueprint(bp, url_prefix='/mantenimientos-tecnologia')
 
 # Donde configuro mi clave
@@ -19,18 +19,14 @@ db.init_app(app)
 login_manager.init_app (app)
 login_manager.login_view = "main.login"
 
-
-
 def status_401(error):
     return redirect(url_for('login'))
-
 
 def status_404(error):
     return "<h1>Página no encontrada</h1>", 404
 
 app.register_error_handler(401, status_401)
 app.register_error_handler(404, status_404)
-
 
 if __name__ == '__main__':
 
